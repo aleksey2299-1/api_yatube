11e086331b69e28d60fb3a8349b142789deba630
aleksey2299-1 <aleksey@Aleksey-PC>
2023-08-07 04:13:17 +0300

    2nd Try

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,5 +4,5 @@ from rest_framework import permissions
 class OwnProfilePermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (obj.author == request.user or
-                request.method in permissions.SAFE_METHODS)
+        return (obj.author == request.user
+                or request.method in permissions.SAFE_METHODS)
